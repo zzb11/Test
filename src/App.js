932f576cdda8c0330c234330ba6450fc3381ea38
zb <>
2019-01-10 22:44:00 +0800

    add antd

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
+import { Button } from 'antd';
+import './App.css';
 class App extends Component {
   render() {
     return (
       <div className="App">
-       hello world
+       <Button type="primary">Button</Button>
       </div>
     );
   }
