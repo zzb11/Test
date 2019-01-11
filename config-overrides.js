a8fdf2908f0a7a5e24b462c5155c7d92ddd6439e
zb <>
2019-01-11 17:43:59 +0800

    add  customize-cra

--- config-overrides.js
+++ config-overrides.js
@@ -1,15 +1,32 @@
-const { injectBabelPlugin } = require('react-app-rewired');
-const rewireLess = require('react-app-rewire-less');
-module.exports = function override(config, env) {
-    // do stuff with the webpack config...
-   config = injectBabelPlugin(
-      ['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }],
-         config,
-    );
-       config = rewireLess.withLoaderOptions({
-         modifyVars: { "@primary-color": "#1DA57A" },
-         javascriptEnabled: true,
-       })(config, env);
+// const { injectBabelPlugin } = require('react-app-rewired');
+// const rewireLess = require('react-app-rewire-less');
+// module.exports = function override(config, env) {
+//     // do stuff with the webpack config...
+//    config = injectBabelPlugin(
+//       ['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }],
+//          config,
+//     );
+//        config = rewireLess.withLoaderOptions({
+//          modifyVars: { "@primary-color": "#1DA57A" },
+//          javascriptEnabled: true,
+//        })(config, env);
 
-    return config;
-  };
+//     return config;
+//   };
+
+const {
+   override,
+   fixBabelImports,
+   addLessLoader,
+ } = require("customize-cra");
+ 
+ 
+ module.exports = override(
+   fixBabelImports("import", {
+     libraryName: "antd", libraryDirectory: "es", style: true // change importing css to less
+   }),
+   addLessLoader({
+     javascriptEnabled: true,
+     modifyVars: { "@primary-color": "#1DA57A" }
+   })
+ );
