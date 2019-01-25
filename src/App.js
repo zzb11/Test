1814f2edfaf80a87d03081deee7bed3b9987853a
zb <>
2019-01-25 13:43:17 +0800

    upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
 import Clock from './components/Clock';
 import ClockHooks from './components/ClockHooks';
+import Upload from './common/Upload/Upload';
 import { Button } from 'antd';
 import './App.css';
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Upload></Upload>
       {/* <Clock endTime={new Date("2019-01-22 15:00:00").getTime()} startTime={new Date("2019-01-22 14:00:00").getTime()}></Clock> */}
-      <ClockHooks num={"dd"}></ClockHooks>
+      {/* <ClockHooks num={"dd"}></ClockHooks> */}
        {/* <Button type="primary">Button</Button> */}
       </div>
     );
